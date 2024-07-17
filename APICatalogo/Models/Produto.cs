78a37586051c3a34ea51cbabd26f4f78b9b730f4
Ítalo Penha <italopenha77@outlook.com>
2024-07-17 18:30:26 -0300

    Validação Personalizada II

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -6,18 +6,18 @@ using System.Text.Json.Serialization;
 namespace APICatalogo.Models;
 
 [Table("Produtos")]
-public class Produto
+public class Produto : IValidatableObject
 {
     [Key]
     public int ProdutoId { get; set; }
 
     [Required(ErrorMessage = "O nome é obrigatório.")]
-    [StringLength(80, ErrorMessage = "O nome deve ter no máximo {1} caracteres.")]
-    [PrimeiraLetraMaiuscula]
+    [StringLength(80, ErrorMessage = "O nome deve ter no máximo {1} e no mínimo {2} caracteres.", MinimumLength = 5)]
+    //[PrimeiraLetraMaiuscula]
     public string? Nome { get; set; }
 
     [Required]
-    [StringLength(10, ErrorMessage = "A descrição deve ter no máximo {1} caracteres.")]
+    [StringLength(300, ErrorMessage = "A descrição deve ter no máximo {1} caracteres.")]
     public string? Descricao { get; set; }
 
     [Required]
@@ -37,4 +37,22 @@ public class Produto
 
     [JsonIgnore]
     public Categoria? Categoria { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(this.Nome))
+        {
+            var primeiraLetra = this.Nome[0].ToString();
+
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                yield return new ValidationResult("A primeira letra do nome do produto deve ser maiúscula.", new[] { nameof(this.Nome) });
+            }
+        }
+
+        if (this.Estoque <= 0)
+        {
+            yield return new ValidationResult("O estoque deve ser maior que zero.", new[] { nameof(this.Estoque) });
+        }
+    }
 }
