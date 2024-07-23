bf218940e7e78bf44cdb13126ba8f2798c202f4c
Ítalo Penha <italopenha77@outlook.com>
2024-07-23 12:17:16 -0300

    Criando a interface e repositório de Categoria

---
+++ APICatalogo/Repositories/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,13 @@
+﻿using APICatalogo.Models;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
+
+namespace APICatalogo.Repositories.Interfaces;
+
+public interface ICategoriaRepository
+{
+    IEnumerable<Categoria> GetCategorias();
+    Categoria GetCategoria(int id);
+    Categoria Create(Categoria categoria);
+    Categoria Update(Categoria categoria);
+    Categoria Delete(int id);
+}
