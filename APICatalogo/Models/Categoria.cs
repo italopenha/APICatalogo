4ee5e233b1cf6a8b9926f28aad3f37dcc2138922
Ítalo Penha <italopenha77@outlook.com>
2024-07-23 12:48:50 -0300

    Adicionando JsonIgnore na classe Categoria

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace APICatalogo.Models;
 
@@ -18,6 +19,7 @@ public class Categoria
     [StringLength(300)]
     public string? ImagemUrl { get; set; }
 
+    [JsonIgnore]
     public ICollection<Produto>? Produtos { get; set; }
 
     public Categoria()
