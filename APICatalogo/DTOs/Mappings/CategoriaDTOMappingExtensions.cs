772e8fdd64f099e339f587c7ab552db1f5103be5
Ítalo Penha <italopenha77@outlook.com>
2024-07-25 20:44:58 -0300

    DTO - Usando métodos de extensão

---
+++ APICatalogo/DTOs/Mappings/CategoriaDTOMappingExtensions.cs
@@ -0,0 +1,44 @@
+﻿using APICatalogo.Models;
+
+namespace APICatalogo.DTOs.Mappings;
+
+public static class CategoriaDTOMappingExtensions
+{
+    public static CategoriaDTO? ToCategoriaDTO(this Categoria categoria)
+    {
+        if (categoria is null)
+            return null;
+
+        return new CategoriaDTO
+        {
+            CategoriaId = categoria.CategoriaId,
+            Nome = categoria.Nome,
+            ImagemUrl = categoria.ImagemUrl
+        };
+    }
+
+    public static Categoria? ToCategoria(this CategoriaDTO categoriaDTO)
+    {
+        if (categoriaDTO is null) return null;
+
+        return new Categoria
+        {
+            CategoriaId = categoriaDTO.CategoriaId,
+            Nome = categoriaDTO.Nome,
+            ImagemUrl = categoriaDTO.ImagemUrl
+        };
+    }
+
+    public static IEnumerable<CategoriaDTO> ToCategoriasDTOList(this IEnumerable<Categoria> categorias)
+    {
+        if (categorias is null || !categorias.Any())
+            return new List<CategoriaDTO>();
+
+        return categorias.Select(categoria => new CategoriaDTO
+        {
+            CategoriaId = categoria.CategoriaId,
+            Nome = categoria.Nome,
+            ImagemUrl = categoria.ImagemUrl
+        }).ToList();
+    }
+}
