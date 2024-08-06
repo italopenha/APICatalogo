9f1115cd99d80363f0f1dd41db5c7cf26855dc54
Ítalo Penha <italopenha77@outlook.com>
2024-08-06 13:00:38 -0300

    Aula 127 - Implementando a autenticação JWT Bearer III

---
+++ APICatalogo/DTOs/RegisterModelDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.DTOs;
+
+public record RegisterModelDTO
+{
+    [Required(ErrorMessage = "User name is required")]
+    public string? UserName { get; set; }
+
+    [EmailAddress]
+    [Required(ErrorMessage = "E-mail is required")]
+    public string? Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    public string? Password { get; set; }
+}
---
+++ APICatalogo/DTOs/ResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace APICatalogo.DTOs;
+
+public record ResponseDTO
+{
+    public string? Status { get; set; }
+
+    public string? Message { get; set; }
+}
