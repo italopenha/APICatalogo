9765015fd4f18e924730ef1fbc9fe62e3ccb2177
Ítalo Penha <italopenha77@outlook.com>
2024-08-28 15:38:43 -0300

    Aula 172 - Criando e configurando o projeto de testes com xUnit

---
+++ APICatalogoxUnitTests/UnitTests/ProdutosUnitTestController.cs
@@ -0,0 +1,43 @@
+﻿using APICatalogo.Context;
+using APICatalogo.DTOs.Mappings;
+using APICatalogo.Repositories;
+using APICatalogo.Repositories.Interfaces;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APICatalogoxUnitTests.UnitTests;
+
+public class ProdutosUnitTestController
+{
+    private IUnitOfWork repository;
+    private IMapper mapper;
+
+    public static DbContextOptions<AppDbContext> dbContextOptions { get; }
+
+    public static string connectionString = "Server=localhost;DataBase=CatalogoDB;Uid=root;Pwd=w30Z!FSs5MiIpw1w";
+
+    static ProdutosUnitTestController()
+    {
+        dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+            .Options;
+    }
+
+    public ProdutosUnitTestController()
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(new ProdutoDTOMappingProfile());
+        });
+
+        mapper = config.CreateMapper();
+
+        var context = new AppDbContext(dbContextOptions);
+        repository = new UnitOfWork(context);
+    }
+}
