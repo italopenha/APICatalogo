7f3837dc1c085069ec800426bf0b575a8111346a
Ítalo Penha <italopenha77@outlook.com>
2024-09-02 10:28:35 -0300

    Aula 177 - Testes Unitários: Testando os métodos Delete

---
+++ APICatalogoxUnitTests/UnitTests/DeleteProdutoUnitTests.cs
@@ -0,0 +1,50 @@
+﻿
+using APICatalogo.Controllers;
+using APICatalogo.DTOs;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APICatalogoxUnitTests.UnitTests;
+
+public class DeleteProdutoUnitTests : IClassFixture<ProdutosUnitTestController>
+{
+    private readonly ProdutosController _controller;
+
+    public DeleteProdutoUnitTests(ProdutosUnitTestController controller)
+    {
+        _controller = new ProdutosController(controller.repository, controller.mapper);
+    }
+
+    [Fact]
+    public async Task DeleteProdutoById_Return_OkResult()
+    {
+        // Arrange
+        var prodId = 2;
+
+        // Act
+        var result = await _controller.DeleteAsync(prodId) as ActionResult<ProdutoDTO>;
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Result.Should().BeOfType<OkObjectResult>();
+    }
+
+    [Fact]
+    public async Task DeleteProdutoById_Return_NotFound()
+    {
+        // Arrange
+        var prodId = 999;
+
+        // Act
+        var result = await _controller.DeleteAsync(prodId) as ActionResult<ProdutoDTO>;
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Result.Should().BeOfType<NotFoundObjectResult>();
+    }
+}
